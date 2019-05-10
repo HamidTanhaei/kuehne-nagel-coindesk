1e4c90d41dfd08e739e8375045d3049290be1f3a
HamidTanhaei <hamidt.com@gmail.com>
2019-05-10 12:35:14 +0430

    Component/LoadingSpinner {warning removed (image alt added)}

--- src/Component/LoadingSpinner/index.jsx
+++ src/Component/LoadingSpinner/index.jsx
@@ -3,6 +3,6 @@ import loadingSpinner from './loading-spinner.svg';
 export default (props) => {
   const size  = props.size ? props.size : '40px';
   return(
-    <img width={size} height={size} src={loadingSpinner} />
+    <img alt="loading..." width={size} height={size} src={loadingSpinner} />
   );
 };
